9c952c6b204ababc84258c20079ecd2feaaacf75
JuliaRees <julia@refinedislandcreations.com>
2024-11-15 15:33:13 +0800

    done

---
+++ scripts.js
@@ -0,0 +1,25 @@
+const filterButtons = document.querySelectorAll(".filterbutton")
+const filterDishes = document.querySelectorAll(".filterdish")
+console.log(filterButtons, filterDishes)
+
+function filterDish() {
+  filterButtons.forEach(button => {
+    button.classList.remove("font-semibold")
+    button.classList.remove("underline")
+  })
+  this.classList.add("font-semibold")
+  this.classList.add("underline")
+  console.log(this.dataset.filter)
+  filterDishes.forEach(dish => {
+    if (dish.classList.contains(this.dataset.filter)){
+      dish.classList.remove("hidden")
+    } else {
+      dish.classList.add("hidden")
+    }
+  })
+}
+
+filterButtons.forEach(button => {
+  
+  button.addEventListener("click", filterDish
+)})
